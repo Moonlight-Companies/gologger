f57740894191783eee6bbb74e8a978d5999af7b1
Chris Given <chris.given@moonlightcompanies.com>
2024-07-22 13:35:14 -0700

    test for each level

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -5,5 +5,8 @@ import "testing"
 func TestLogger(t *testing.T) {
 	t.Run("TestLogger", func(t *testing.T) {
 		Log.Infoln("This is an info message")
+		Log.Warnln("This is a warning message")
+		Log.Errorln("This is an error message")
+		Log.Debugln("This is a debug message")
 	})
 }
